a847efa4fc2eae4461797f4b69677a5bd5febe46
matiasba8 <matias.bustos@redcomercio.cl>
2023-02-28 20:47:28 -0300

    styles

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -3,10 +3,12 @@ import './add_jquery'
 
 $('body').on('click', '.toggle-menu', function(){
   console.log("toggle");
-  $('#navbar').toggleClass('mobile-navbar');
-  $('.btn-mobile-nav').toggleClass('active');
-  $('.close-menu').show();
-  $(this).hide();
+  $('#navbar').toggleClass('mobile-navbar');  // for styles
+  $('.btn-mobile-nav').toggleClass('active'); // for styles
+  $('.close-menu').show();                    // show close btn
+  $(this).hide();                             // hide menu
+
+  $('.usefull-links').attr('id', 'mobile');
 });
 
 
@@ -16,4 +18,8 @@ $('body').on('click', '.close-menu', function(){
   $('.btn-mobile-nav').toggleClass('active');
   $('.toggle-menu').show();
   $(this).hide();
+
+  // if (!($(window).width() > 530)){
+  // }
+  $('.usefull-links').attr('id', '');
 });
