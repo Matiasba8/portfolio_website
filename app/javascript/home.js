eea75be7924275d7110f941a96d2afb1b34a2848
matiasba8 <matias.bustos@redcomercio.cl>
2023-03-05 12:56:44 -0300

    when the windows width reach <= 465px we transform year experience label to only his number

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -2,7 +2,6 @@ import './add_jquery'
 
 
 $('body').on('click', '.toggle-menu', function(){
-  console.log("toggle");
   $('#navbar').toggleClass('mobile-navbar');  // for styles
   $('.btn-mobile-nav').toggleClass('active'); // for styles
   $('.close-menu').show();                    // show close btn
@@ -13,13 +12,29 @@ $('body').on('click', '.toggle-menu', function(){
 
 
 $('body').on('click', '.close-menu', function(){
-  console.log("close");
   $('#navbar').toggleClass('mobile-navbar');
   $('.btn-mobile-nav').toggleClass('active');
   $('.toggle-menu').show();
   $(this).hide();
 
-  // if (!($(window).width() > 530)){
-  // }
+
   $('.usefull-links').attr('id', '');
+});
+
+$(window).on('resize load', function(){
+  var win = $(this); //this = window
+  if (win.width() <= 465) { 
+
+    $('.year-label').remove();
+
+  }
+  else{
+    let year_label = $('.years-of-experience').find('.year-label')
+    let container_year_exp = $('.years-of-experience')
+
+    if (!year_label.length){ // not exist
+      console.log("add year of experience label")
+      container_year_exp.append('<div class="year-label">year experience</div>')
+    }
+  }
 });
