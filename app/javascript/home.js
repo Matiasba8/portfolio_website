070f525c370d1fb4fdc6437dbe0c42b0bb2d1484
matiasba8 <matias.bustos@redcomercio.cl>
2023-03-08 14:07:12 -0300

    some icons styles for programming languages section

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -1,6 +1,6 @@
 import './add_jquery'
 
-
+// Toggle and untogle mobile navigations
 $('body').on('click', '.toggle-menu', function(){
   $('#navbar').toggleClass('mobile-navbar');  // for styles
   $('.btn-mobile-nav').toggleClass('active'); // for styles
@@ -21,6 +21,7 @@ $('body').on('click', '.close-menu', function(){
   $('.usefull-links').attr('id', '');
 });
 
+// On resize less that 465 screen with we just delete the years experience, and when is more than 465 screen width we just added it back again
 $(window).on('resize load', function(){
   var win = $(this); //this = window
   if (win.width() <= 465) { 
@@ -33,8 +34,28 @@ $(window).on('resize load', function(){
     let container_year_exp = $('.years-of-experience')
 
     if (!year_label.length){ // not exist
-      console.log("add year of experience label")
       container_year_exp.append('<div class="year-label">year experience</div>')
     }
   }
+});
+
+
+// On mouse over ruby we just add col-12
+$(".ruby-logo").on("mouseover", function () {
+    $('.ruby-card-cc').addClass('active');
+});
+$(".ruby-logo").on("mouseleave", function () {
+  setTimeout(function() {
+    $('.ruby-card-cc').removeClass('active');
+}, 800);
+});
+
+// On mouse over ruby we just add col-12
+$(".javascript-logo").on("mouseover", function () {
+  $('.javascript-card-cc').addClass('active');
+});
+$(".javascript-logo").on("mouseleave", function () {
+setTimeout(function() {
+  $('.javascript-card-cc').removeClass('active');
+}, 800);
 });
